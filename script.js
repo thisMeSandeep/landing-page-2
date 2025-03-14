634c7e62ed13924fcafee33523986493d4386343
Sandeep Singh <snayal50@gmail.com>
2025-03-14 10:55:55 +0530

    make mobile responsive

---
+++ script.js
@@ -0,0 +1,27 @@
+//review slider
+let swiper = new Swiper(".reviews-container", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+  },
+});
